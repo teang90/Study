1d5646adb8ca95716308f1aac810c14902de4ff1
teang90 <User@B-14>
2020-07-14 16:43:36 +0900

    124나라 문제 풀이, 스트림 예제

--- Study/src/algorithm/solutions/programmers/practice/Level2/NationOf124_124나라.java
+++ Study/src/algorithm/solutions/programmers/practice/Level2/NationOf124_124나라.java
@@ -4,21 +4,55 @@ public class NationOf124_124나라 {
 	// https://programmers.co.kr/learn/courses/30/lessons/12899
 	
 	public static void main(String[] args) {
-		int n = 0;
-		solution(n);
+		int n = 1;
+//		int[] inArrary = {15};
+		int[] inArrary = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,20};
+		for (int i : inArrary) {
+			String answer = solution(i);
+			System.out.println("answer : "+answer);
+		}
 	}
 
+	//재귀 사용하지 않고...
 	public static String solution(int n) {
 		String answer = "";
-		int r = n%3;
-		r = (r==0)?4:(r==1?1:2);
-		
-		// TODO 1) 몫 구해서 124로 분기처리 & 수에 따라서 어떻게 처리할지...
-		// 		2) 가능하면 재귀적 형태로 처리할 수 있도록...
-		int q = n/3;
-		q = (q==0)?4:(q==1?1:2);
-		
-		return answer;
+		int q = n;
+		while (true) {
+			int r = q%3==0?4:q%3;
+			answer=r+answer;
+			q=(r==4?(q/3)-1:q/3);
+			if (q<=3) {
+				answer = (q==3?4:q)+answer;
+				break; 
+			} 
+		}
+		return answer.replace("0", "");
 	}
 
+//	/** 재귀 사용 -> 효율성 fail */
+//	public static String solution(int n) {
+//		return String.valueOf(recur(n));
+//	}
+//	
+//	// 재귀로 뱉는 함수...
+//	public static int recur(int n) {
+//		String res = "";
+//		res = (n%3==0)?res+"4":res+n%3;
+//		if(n > 3) res=(n%3==0?recur(n/3 - 1):recur(n/3))+res;
+//		return Integer.parseInt(res);
+//	}
 }
+
+/** 다른사람 풀이 */
+//class Solution {
+//  public String solution(int n) {
+//      String[] num = {"4","1","2"};
+//      String answer = "";
+//
+//      while(n > 0){
+//          answer = num[n % 3] + answer;
+//          n = (n - 1) / 3;
+//      }
+//      return answer;
+//  }
+//}
